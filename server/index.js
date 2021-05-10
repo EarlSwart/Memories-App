0f2fb69577f1c7fa0ea7a590e171ccb5acfcacaa
EarlSwart <swartearl@gmail.com>
2021-05-10 15:51:44 +0200

    App completed. Update, delete and like functionality added.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
-
+dotenv.config()
+;
 
 app.use(express.json({ limit: '30mb', extended: 'true' }))
 app.use(express.urlencoded({ extended: true }));
@@ -15,10 +15,9 @@ app.use(cors());
 app.use('/posts', postRoutes)
 
 // https://cloud.mongodb.com/v2/6093ccd26fc9157b61020191#clusters
-const CONNECTION_URL = 'mongodb+srv://admin-earl:test123@cluster0.cko3h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
     .catch((error) => console.log(error));
 
