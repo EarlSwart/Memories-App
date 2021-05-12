b8fb47c93f26112e670c0e1c1ff7116727a5140d
EarlSwart <swartearl@gmail.com>
2021-05-12 12:54:37 +0200

    Auth Middleware added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -25,7 +25,7 @@ export const updatePost = async (req, res) => {
     const { id: _id } = req.params;
     const post = req.body
 
-    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id.');
 
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
 
@@ -35,20 +35,31 @@ export const updatePost = async (req, res) => {
 export const deletePost = async (req, res) => {
     const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id.');
 
     await PostMessage.findByIdAndRemove(id);
 
-    res.json({ message: 'Post deleted successfully' });
+    res.json({ message: 'Post deleted successfully.' });
 }
 
 export const likePost = async (req, res) => {
     const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+    if (!req.userId) return res, json({ message: 'Unauthenticated' })
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id.');
 
     const post = await PostMessage.findById(id);
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+    const index = post.likes.findIndex((id) => id === String(req.userId));
+
+    if (index === -1) {
+        post.likes.push(req.userId);
+    } else {
+        post.likes = post.likess.filter((id) => id !== String(req.userId));
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
 
     res.json(updatedPost)
 }
